aecefa4d41ea4edb7f5beb46ff88c03fea9eaedb
ARAGAO527 <00001134669185sp@al.educacao.sp.gov.br>
2026-03-30 12:27:53 -0300

    Add files via upload

---
+++ verificador.py
@@ -0,0 +1,6 @@
+#--verificador de Login--#
+usuario = input("Digite o nome de usuário: ")
+if usuario == "adm":
+    print("Acesso Total Liberado")
+else:
+    print("Acesso Limitado Concedido")    
